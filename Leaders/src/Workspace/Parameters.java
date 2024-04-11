e123eaf8d03d1543333f782a1d1d2a31bd0c0f63
suzan99gh <suzan.gh99@gmail.com>
2024-04-11 19:34:39 +0300

    first uplod

---
+++ Leaders/src/Workspace/Parameters.java
@@ -0,0 +1,14 @@
+package Workspace;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Parameters {
+WebDriver driver = new ChromeDriver();
+String URl = "https://leaders.jo/en/product-category/all-products/electronics-en/mobiles-accessories/?filters=product_cat%5Bmobiles%5D";
+
+Random rand = new Random();
+
+}
